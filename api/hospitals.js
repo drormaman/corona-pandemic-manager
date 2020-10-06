4887d2ccf64ee60781e532185806284b2198dc96
DrorMaman <drormaman@gmail.com>
2020-10-06 21:43:45 +0300

    hospital route done
    tests passed

--- api/hospitals.js
+++ api/hospitals.js
@@ -1,11 +1,29 @@
 const { Router } = require("express");
 const router = Router();
-const {
-	Patients,
-	Symptoms,
-	Cities,
-	Hospitals,
-	CovidTests
-} = require("../models");
+const { Patients, Hospitals } = require("../models");
+
+router.get("/", async (req, res) => {
+	const hospitals = await Hospitals.findAll();
+	res.json(hospitals);
+});
+
+router.get("/byId/:hospitalId", async (req, res) => {
+	const hospital = await Hospitals.findByPk(req.params.hospitalId);
+	res.json(hospital);
+});
+router.get("/respirator_luck", async (req, res) => {
+	const hospitals = await Hospitals.findAll({
+		include: [
+			{
+				model: Patients,
+				where: { status: "respiratory" }
+			}
+		]
+	});
+	const lucky = hospitals.filter(hospital => {
+		return hospital.respiratorAmount - hospital.Patients.length < 5;
+	});
+	res.json(lucky);
+});
 
 module.exports = router;
--- api/index.js
+++ api/index.js
@@ -2,5 +2,6 @@ const { Router } = require("express");
 const router = Router();
 
 router.use("/patients", require("./patients.js"));
+router.use("/hospitals", require("./hospitals.js"));
 
 module.exports = router;
